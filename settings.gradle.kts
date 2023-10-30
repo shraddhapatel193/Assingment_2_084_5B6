f0b48a92fc8f57a17289681d2e82c79c23de845e
shraddha <shraddhabpatel21@gnu.ac.in>
2023-10-30 22:27:19 +0530

    Assingment-2

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Assingment_2_084_5b6"
+include(":app")
+ 
